6a5a0bdc8c2431c516f2e5daf0e2a62136374ef9
Yulia <60868878+yulia9112@users.noreply.github.com>
2020-12-02 00:46:57 +0300

    Update MenuLevels.java

--- APP/MenuLevels.java
+++ APP/MenuLevels.java
@@ -6,8 +6,11 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+
 
 public class MenuLevels extends AppCompatActivity {
 
@@ -16,18 +19,60 @@ public class MenuLevels extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_levels);
 
-        //Button buttonStart = (Button)findViewById(R.id.buttonStart); //объект для кнопки начать
-        //buttonStart.setOnClickListener(new View.OnClickListener() {
-          //  @Override
-           // public void onClick(View view) {
-             //   try {
-               //     Intent intent = new Intent(MenuLevels.this, Menu1.class);
-                 //   startActivity(intent); finish();     //команда перейти на другую страницу
-               // } catch (Exception e) {  //если переход не состоится, то игра не вылетит и не закроется
+        ImageButton imageButtonBack = (ImageButton)findViewById(R.id.imageButtonBack);
+        imageButtonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent (MenuLevels.this, Menu1.class);
+                    startActivity(intent); finish();
+                }catch (Exception e) {
 
-               // }
-           // }
-        //});
+                }
+            }
+        });
+
+        Button buttonLite = (Button)findViewById(R.id.buttonLite); //объект для кнопки
+        buttonLite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new Intent(MenuLevels.this, LiteLevel.class);
+                    startActivity(intent);
+                    finish();     //команда перейти на другую страницу
+                } catch (Exception e) {  //если переход не состоится, то игра не вылетит и не закроется
+
+                }
+            }
+        });
+
+        Button buttonMedium = (Button)findViewById(R.id.buttonMedium); //объект для кнопки
+        buttonMedium.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new Intent(MenuLevels.this, MediumLevel.class);
+                    startActivity(intent);
+                    finish();     //команда перейти на другую страницу
+                } catch (Exception e) {  //если переход не состоится, то игра не вылетит и не закроется
+
+                }
+            }
+        });
+
+        Button buttonHard = (Button)findViewById(R.id.buttonHard); //объект для кнопки
+        buttonHard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new Intent(MenuLevels.this, HardLevel.class);
+                    startActivity(intent);
+                    finish();     //команда перейти на другую страницу
+                } catch (Exception e) {  //если переход не состоится, то игра не вылетит и не закроется
+
+                }
+            }
+        });
 
         Window w;
         w = getWindow();
